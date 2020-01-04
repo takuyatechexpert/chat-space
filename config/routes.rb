58ba70b578869ba8c7e77e2f266c3813333eae10
takuyatechexpert <lp4546.sg.abc@gimail.com>
2020-01-04 12:28:16 +0900

    add messages Send function
    add flash message
    add message view
    add gem carrierwave

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   
-  resources :messages, only: :index
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:edit, :update, :new, :create, :index]
+  resources :users, only: [:edit, :update, :index]
+  resources :groups, only: [:edit, :update, :new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
